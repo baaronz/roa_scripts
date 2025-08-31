f70aed9f735240f44c5c74e6c69d14ade2ecefa5
baaronz <baaronz.aaron1@gmail.com>
2025-08-31 17:37:10 -0400

    Update ROA_custom_quest.lua

--- Goldshire/ROA_custom_quest.lua
+++ Goldshire/ROA_custom_quest.lua
@@ -1,6 +1,5 @@
 local QUEST_ID = {64070, 64071, 64072, 64073, 64074, 64075, 64076, 64077, 64078, 64079, 64080, 64081, 64082, 64083, 64084, 64085, 64086, 64087, 64088, 64089, 64090, 64091, 64092, 64093, 64094, 64095, 64096, 64097, 64098}
 
-
 local function KilledByCreature(event, killer, killed)
     for _, questId in ipairs(QUEST_ID) do
         if killed:HasQuest(questId) then
