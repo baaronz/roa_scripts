682686dec60a4b118a64deb5d56b731073007a2b
baaronz <baaronz.aaron1@gmail.com>
2025-08-31 17:59:42 -0400

    Update ROA_bank.lua

--- Goldshire/ROA_bank.lua
+++ Goldshire/ROA_bank.lua
@@ -1,10 +1,7 @@
-
 local command = "bank"
 
 function OnEvents(event, player, message, type, language)
-
     if (message == command) then
-		
        player:SendBroadcastMessage("|cff00cc00Welcome to your bank, lazy|cff00ffff ["..player:GetName().."]|r")
 	   player:SendShowBank(player)
 	   return false;
