653e6079b8f109d7fd965e23c8357d8e5a58d916
baaronz <baaronz.aaron1@gmail.com>
2025-08-31 17:40:17 -0400

    Update ROA_disabled_zones.lua

--- Goldshire/ROA_disabled_zones.lua
+++ Goldshire/ROA_disabled_zones.lua
@@ -1,182 +1,78 @@
+local function SendMessage(player)
+    player:SendBroadcastMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
+    player:SendBroadcastMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
+    player:SendAreaTriggerMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
+    player:SendAreaTriggerMessage("|c979ABDFFYou tried to enter a restricted zone.|r") 
+    player:Teleport(1, -7161.2, -3808.1, 9.2, 0.67) -- Tanaris tp
+end
+
 local function OnMapChange(event, player)
     if player:IsGM() == true then
         return
     end
 
     if player:GetZoneId() == 3483 then
-
-        player:SendBroadcastMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        player:SendBroadcastMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-
-        player:SendAreaTriggerMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-        player:SendAreaTriggerMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        
-        player:Teleport(1, -7161.2, -3808.1, 9.2, 0.67)
+        SendMessage(player)
     end
 
     if player:GetZoneId() == 3521 then
-        
-        player:SendBroadcastMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        player:SendBroadcastMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-
-        player:SendAreaTriggerMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-        player:SendAreaTriggerMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        
-        player:Teleport(1, -7161.2, -3808.1, 9.2, 0.67)
+        SendMessage(player)
     end
 
     if player:GetZoneId() == 3522 then
-        
-        player:SendBroadcastMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        player:SendBroadcastMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-
-        player:SendAreaTriggerMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-        player:SendAreaTriggerMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        
-        player:Teleport(1, -7161.2, -3808.1, 9.2, 0.67)
+        SendMessage(player)
     end
 
-    if player:GetZoneId() == 3523 then
-        
-        player:SendBroadcastMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        player:SendBroadcastMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-
-        player:SendAreaTriggerMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-        player:SendAreaTriggerMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        
-        player:Teleport(1, -7161.2, -3808.1, 9.2, 0.67)
+    if player:GetZoneId() == 3523 then 
+        SendMessage(player)
     end
 
     if player:GetZoneId() == 3518 then
-        
-        player:SendBroadcastMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        player:SendBroadcastMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-
-        player:SendAreaTriggerMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-        player:SendAreaTriggerMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        
-        player:Teleport(1, -7161.2, -3808.1, 9.2, 0.67)
+        SendMessage(player)
     end
 
     if player:GetZoneId() == 3519 then
-        
-        player:SendBroadcastMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        player:SendBroadcastMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-
-        player:SendAreaTriggerMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-        player:SendAreaTriggerMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        
-        player:Teleport(1, -7161.2, -3808.1, 9.2, 0.67)
+        SendMessage(player)
     end
 
     if player:GetZoneId() == 3520 then
-        
-        player:SendBroadcastMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        player:SendBroadcastMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-
-        player:SendAreaTriggerMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-        player:SendAreaTriggerMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        
-        player:Teleport(1, -7161.2, -3808.1, 9.2, 0.67)
+        SendMessage(player)
     end
 
     if player:GetZoneId() == 3537 then
-        
-        player:SendBroadcastMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        player:SendBroadcastMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-
-        player:SendAreaTriggerMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-        player:SendAreaTriggerMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        
-        player:Teleport(1, -7161.2, -3808.1, 9.2, 0.67)
+        SendMessage(player)
     end
 
     if player:GetZoneId() == 3711 then
-        
-        player:SendBroadcastMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        player:SendBroadcastMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-
-        player:SendAreaTriggerMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-        player:SendAreaTriggerMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        
-        player:Teleport(1, -7161.2, -3808.1, 9.2, 0.67)
+        SendMessage(player)
     end
 
     if player:GetZoneId() == 210 then
-        
-        player:SendBroadcastMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        player:SendBroadcastMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-
-        player:SendAreaTriggerMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-        player:SendAreaTriggerMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        
-        player:Teleport(1, -7161.2, -3808.1, 9.2, 0.67)
+        SendMessage(player)
     end
 
     if player:GetZoneId() == 67 then
-        
-        player:SendBroadcastMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        player:SendBroadcastMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-
-        player:SendAreaTriggerMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-        player:SendAreaTriggerMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        
-        player:Teleport(1, -7161.2, -3808.1, 9.2, 0.67)
+        SendMessage(player)
     end
 
     if player:GetZoneId() == 66 then
-        
-        player:SendBroadcastMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        player:SendBroadcastMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-
-        player:SendAreaTriggerMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-        player:SendAreaTriggerMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        
-        player:Teleport(1, -7161.2, -3808.1, 9.2, 0.67)
+        SendMessage(player)
     end
 
     if player:GetZoneId() == 394 then
-        
-        player:SendBroadcastMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        player:SendBroadcastMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-
-        player:SendAreaTriggerMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-        player:SendAreaTriggerMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        
-        player:Teleport(1, -7161.2, -3808.1, 9.2, 0.67)
+        SendMessage(player)
     end
 
     if player:GetZoneId() == 495 then
-        
-        player:SendBroadcastMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        player:SendBroadcastMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-
-        player:SendAreaTriggerMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-        player:SendAreaTriggerMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        
-        player:Teleport(1, -7161.2, -3808.1, 9.2, 0.67)
+        SendMessage(player)
     end
 
     if player:GetZoneId() == 3703 then
-        
-        player:SendBroadcastMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        player:SendBroadcastMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-
-        player:SendAreaTriggerMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-        player:SendAreaTriggerMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        
-        player:Teleport(1, -7161.2, -3808.1, 9.2, 0.67)
+        SendMessage(player)
     end
 
     if player:GetZoneId() == 4395 then
-        
-        player:SendBroadcastMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        player:SendBroadcastMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-
-        player:SendAreaTriggerMessage("|c979ABDFFYou've been teleported to a faction neutral zone.|r")
-        player:SendAreaTriggerMessage("|c979ABDFFYou tried to enter a restricted zone.|r")
-        
-        player:Teleport(1, -7161.2, -3808.1, 9.2, 0.67)
+        SendMessage(player)
     end
 end
 
