09cfdb59e5cc929a9d1da600b5922f860d41b27e
baaronz <baaronz.aaron1@gmail.com>
2025-09-05 14:29:59 -0400

    Update ROA_gobj_custom.lua

--- Goldshire/ROA_gobj_custom.lua
+++ Goldshire/ROA_gobj_custom.lua
@@ -7,9 +7,20 @@ end
 local function OnGossipSelect(event, player, object, sender, intid, code, menu_id)
     local gobjguid = object:GetEntry()
     local gobjguid1 = tostring(gobjguid)
+
+    local posx = WorldDBQuery("SELECT position_x FROM gameobject WHERE guid = " .. gobjguid1)
+    local posy = WorldDBQuery("SELECT position_y FROM gameobject WHERE guid = " .. gobjguid1)
+    local posz = WorldDBQuery("SELECT position_z FROM gameobject WHERE guid = " .. gobjguid1)
+    local poso = WorldDBQuery("SELECT orientation FROM gameobject WHERE guid = " .. gobjguid1)
+
+    local posx1 = math.floor(posx + 0.01)
+
     if intid == 1 then
         player:SendBroadcastMessage("You have placed your palm on the stone")
         player:SendBroadcastMessage("Gobj GUID: " .. gobjguid1)
+        player:SendBroadcastMessage("PosX: " .. posx .. "PosY: " .. posy .. "PosZ: " .. posz .. "PosO: " .. poso)
+        player:SendBroadcastMessage("PosX1: " .. posx1)
+        object:SpawnCreature(63003, posx1, posy, posz, poso, 6, 20)
     end
     player:GossipComplete()
 end
