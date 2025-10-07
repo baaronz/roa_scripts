3642b033eedb9630ffe63a8f4df3b0a9293dbccc
baaronz <baaronz.aaron1@gmail.com>
2025-10-07 19:50:13 -0400

    Update ROA_buffnpc.lua

--- lua_scripts/ROA_buffnpc.lua
+++ lua_scripts/ROA_buffnpc.lua
@@ -1,14 +1,15 @@
-local npc_id = 9000038
+local npc_id = 100006
 
 -- Array of buff spell IDs to apply to the party/raid
 local BUFF_SPELLS = {
+    16609, -- Warchiefs Blessing
     22888, -- Rallying Cry
     24425  -- Spirit of Zandalar
 }
 
 local function OnHello(event, player, object)
     player:GossipClearMenu()
-    player:GossipMenuAddItem(2, "Buff my party", 0, 1)
+    player:GossipMenuAddItem(2, "Buff my party (50 gold)", 0, 1)
     player:GossipMenuAddItem(0, "Goodbye.", 0, 99)
     player:GossipSendMenu(1, object, 1)
 end
@@ -48,10 +49,18 @@ end
 
 local function OnSelect(event, player, object, sender, intid, code, menu_id)
     if intid == 1 then
+		local money = player:GetCoinage()
         local groupMembers = GetGroupMembers(player)
         local totalBuffsApplied = 0
         local buffedPlayers = 0
-        
+		
+		if money < 500000 then
+            player:SendBroadcastMessage("|c979ABDFFNot enough coins to buff your party.|r")
+			return
+		end
+		
+        player:ModifyMoney(-500000)
+		
         for _, member in ipairs(groupMembers) do
             if member and member:IsInWorld() then
                 local buffsApplied = ApplyBuffsToPlayer(member, object)
